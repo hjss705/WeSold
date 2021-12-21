05d2216ff08f62a82755f2a4a56d8e1c47daabfb
sunmin <smlee950118@naver.com>
2021-12-21 17:14:44 +0900

    2021-12-21 commit!

--- app/src/main/java/com/example/wesold/SubActivity.java
+++ app/src/main/java/com/example/wesold/SubActivity.java
@@ -1,7 +1,11 @@
 package com.example.wesold;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 public class SubActivity extends AppCompatActivity {
@@ -9,5 +13,16 @@ public class SubActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //닉네임규정 버튼 클릭시 액티비티 전환
+        Button name_rule_btn = (Button) findViewById(R.id.login_main);
+        name_rule_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), Sub2Activity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
