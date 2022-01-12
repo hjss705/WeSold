781492478bc1c20d11d3113145a0d90b4aae2189
sunmin <smlee950118@naver.com>
2022-01-12 15:38:24 +0900

    [위솔드] firebase - 데이터베이스연동추가

--- app/src/main/java/com/example/wesold/SubActivity.java
+++ app/src/main/java/com/example/wesold/SubActivity.java
@@ -6,11 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 public class SubActivity extends AppCompatActivity {
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
 
