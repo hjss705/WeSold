7d16b57cbc81cf0c1e4fe72bcbf9b11eb58ab2cc
sunmin <smlee950118@naver.com>
2022-01-13 13:42:50 +0900

    [위솔드] 로그인기능추가

--- app/src/main/java/com/example/wesold/SubActivity.java
+++ app/src/main/java/com/example/wesold/SubActivity.java
@@ -1,27 +1,93 @@
 package com.example.wesold;
 
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SubActivity extends AppCompatActivity {
+    Button  mLoginBtn;
+    TextView mResigettxt;
+    EditText mEmailText, mPasswordText;
+    private FirebaseAuth firebaseAuth;
+//    ProgressDialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login2);
+        setContentView(R.layout.activity_login);
 
-        //닉네임규정 버튼 클릭시 액티비티 전환
-        Button name_rule_btn = (Button) findViewById(R.id.login_main);
-        name_rule_btn.setOnClickListener(new View.OnClickListener(){
+
+        firebaseAuth =  FirebaseAuth.getInstance();
+        //버튼 등록하기
+        mResigettxt = findViewById(R.id.register_t2);
+        mLoginBtn = findViewById(R.id.login_main);
+        mEmailText = findViewById(R.id.emailEt);
+        mPasswordText = findViewById(R.id.passwordEdt);
+//        progressDialog = new ProgressDialog(this);
+
+
+        //가입 버튼이 눌리면
+        mResigettxt.setOnClickListener(new View.OnClickListener(){
+
             @Override
-            public void onClick(View view){
-                Intent intent = new Intent(getApplicationContext(), Sub2Activity.class);
-                startActivity(intent);
+            public void onClick(View v) {
+                //intent함수를 통해 register액티비티 함수를 호출한다.
+                startActivity(new Intent(SubActivity.this,SignUpActivity.class));
+
             }
         });
 
+        //로그인 버튼이 눌리면
+        mLoginBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                String email = mEmailText.getText().toString().trim();
+                String pwd = mPasswordText.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(SubActivity.this, "email을 입력해 주세요.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(pwd)){
+                    Toast.makeText(SubActivity.this, "password를 입력해 주세요.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+//                progressDialog.setMessage("로그인중입니다. 잠시 기다려 주세요...");
+//                progressDialog.show();
+
+
+                firebaseAuth.signInWithEmailAndPassword(email,pwd)
+                        .addOnCompleteListener(SubActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Intent intent = new Intent(SubActivity.this, Sub2Activity.class);
+                                    startActivity(intent);
+
+                                }else{
+                                    Toast.makeText(SubActivity.this,"로그인 오류", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+            }
+        });
     }
 }
