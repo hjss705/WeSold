ea9a9efc8b652c369f53f3f1b9d82f15efbdc44d
sunmin <smlee950118@naver.com>
2022-01-18 15:07:08 +0900

    [위솔드] 네이버맵 추가

--- app/src/main/java/com/example/wesold/NaverMap.java
+++ app/src/main/java/com/example/wesold/NaverMap.java
@@ -1,15 +1,20 @@
 package com.example.wesold;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.naver.maps.map.MapView;
+
 public class NaverMap extends AppCompatActivity {
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navermap);
+
+        mapView = findViewById(R.id.map_View);
+        mapView.onCreate(savedInstanceState);
     }
 }
 
